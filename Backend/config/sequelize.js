8c760204f01dd6de61b3957cdef224aa12d5e256
Samuel Iyiola <Samueliyiola13@gmail.com>
2025-01-28 15:55:53 +0100

    Initial Commit

--- Backend/config/sequelize.js
+++ Backend/config/sequelize.js
@@ -1,6 +1,7 @@
+// Initialize Sequelize ORM For MySQL
 const {Sequelize} = require("sequelize");
 
-const sequelize = Sequelize.define(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host : process.env.HOST,
     dialect : "mysql",
     logging : true
