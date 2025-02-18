1bd1618baa833a4cbafbca546e04c47548ba1de1
Samuel Iyiola <Samueliyiola13@gmail.com>
2025-02-18 23:03:59 +0100

    feat: added file upload functionality

--- Backend/routes/teacherRoutes.js
+++ Backend/routes/teacherRoutes.js
@@ -1,11 +1,17 @@
-const {registerTeacher, loginTeacher} = require('../controllers/teacher.js');
+const {registerTeacher, loginTeacher, uploadFile} = require('../controllers/teacher.js');
+// const {uploadFile} = require('../controllers/teacher.js');
+const upload = require("../config/multerConfig.js");
+const {verifyTeacher}= require("../middlewares/verifyTeacher.js");
 const express = require("express");
 
 const router = express.Router();
 
 router.post("/register", registerTeacher);
 router.post("/login", loginTeacher);
+// router.post("/upload", verifyTeacher, upload.single("file"), uploadFile);
 
+// router.post("/upload", verifyTeacher, uploadFile);
 
+router.post("/upload", verifyTeacher, upload.single("file"), uploadFile);
 
 module.exports = router;
