c7dd7ae1038f10d9bef7b2d3df6916b30c2870a9
Temi <148563288+afroBalogun@users.noreply.github.com>
2025-03-31 22:03:07 +0100

    Update baseUrl.js

--- Frontend/src/utils/baseUrl.js
+++ Frontend/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     return process.env.NODE_ENV === "production"
-    ? "https://citadel-missionary-form.onrender.com"
+    ? "https://group-2-419-lms.onrender.com"
     : "http://localhost:5000";
 };
 
